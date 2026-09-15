775375a788aae104d2f7867606df05a0253960da
hugh dwyer <hugh.dwyer+1789505182@dbtlabsworkshop.com>
2026-09-15 21:49:02 +0000

    fix(dbt v2): dbt wizard identified updates

--- models/marts/agg_payment_method_mix.sql
+++ models/marts/agg_payment_method_mix.sql
@@ -27,5 +27,5 @@ from (
 )
 pivot (
     sum(collected_gold)
-    for primary_payment_method in (any order by primary_payment_method)
+    for primary_payment_method in ('coin', 'guild_credit')
 ) as pivoted
