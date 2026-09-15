f360680f31a060787fbb6f15b17d0aace19b0165
austin harrison <austin.harrison@dbtlabsworkshop.com>
2026-09-15 21:51:38 +0000

    Fix strict static-analysis errors.

--- models/marts/agg_payment_method_mix.sql
+++ models/marts/agg_payment_method_mix.sql
@@ -27,5 +27,5 @@ from (
 )
 pivot (
     sum(collected_gold)
-    for primary_payment_method in (any order by primary_payment_method)
+    for primary_payment_method in ('coin', 'guild_credit', 'crystal_transfer', 'barter')
 ) as pivoted
