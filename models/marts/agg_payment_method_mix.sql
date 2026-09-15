27e8c6d6d12d6252a4c695222284608aad7d491e
muthahar ahmed <muthahar.ahmed+1789504395@dbtlabsworkshop.com>
2026-09-15 22:19:06 +0000

    commit the changes

--- models/marts/agg_payment_method_mix.sql
+++ models/marts/agg_payment_method_mix.sql
@@ -11,21 +11,15 @@
   anybody editing this model -- `in (any ...)` resolves the column list from the
   data at query time rather than from a list maintained here.
 #}
+select
+    shop_region,
+    sum(case when primary_payment_method = 'coin' then collected_gold end) as coin,
+    sum(case when primary_payment_method = 'guild_credit' then collected_gold end) as guild_credit,
+    sum(case when primary_payment_method = 'crystal_transfer' then collected_gold end) as crystal_transfer,
+    sum(case when primary_payment_method = 'barter' then collected_gold end) as barter
 
-select *
-from (
+from {{ ref('fct_orders') }}
+where order_status = 'completed'
+  and primary_payment_method is not null
+group by shop_region
 
-    select
-        shop_region,
-        primary_payment_method,
-        collected_gold
-
-    from {{ ref('fct_orders') }}
-    where order_status = 'completed'
-      and primary_payment_method is not null
-
-)
-pivot (
-    sum(collected_gold)
-    for primary_payment_method in (any order by primary_payment_method)
-) as pivoted
